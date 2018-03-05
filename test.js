797c6573b98144bc330d3fe947feb244ef70f983
v.divya110 <divya.venkataramani@shorelineiot.com>
2018-03-05 15:14:15 +0530

    Added unit test for #14

--- test.js
+++ test.js
@@ -48,3 +48,43 @@ test('restore', function (t) {
     })
   })
 })
+
+test('outgoing update after enqueuing a possible offline message', function (t) {
+  var db = memdb()
+  var instance = persistence(db)
+  var client = {
+    clientId: 'abcde'
+  }
+
+  var client1 = {
+    id: 'abcde'
+  }
+
+  var packet = {
+    cmd: 'publish',
+    brokerId: 'adasdasd',
+    brokerCounter: 0,
+    topic: 'test',
+    payload: 'Return of the Jedi',
+    messageId: 7
+  }
+
+  var updatePacket = {
+    cmd: 'pubrel',
+    messageId: 7
+  }
+  
+  // Enqueue an offline packet
+  instance.outgoingEnqueue(client, packet, function (err, packet1) {
+    t.error(err)
+    // When the client comes back online, aedes calls emptyQueue which calls outgoingUpdate
+    instance.outgoingUpdate(client1, packet, function (err, client, packet) {
+      t.notOk(err, 'no error')
+      // When pubrel is published, outgoingUpdate is called again without the broker Id
+      instance.outgoingUpdate(client, updatePacket, function (err, client, packet) {
+        t.notOk(err, 'no error')
+        instance.destroy(t.end.bind(t))
+      })
+    })
+  })
+})
